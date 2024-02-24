55a5dca2a82a3f7d668020f8b69a58ba803028f6
eysssss <102285503+acethecoder-web@users.noreply.github.com>
2024-02-24 06:41:47 -0800

    Add files via upload

---
+++ MOTO_SHOP/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".dropdown-trigger").dropdown();
+  $('.parallax').parallax();
+  $('.carousel').carousel({
+    dist: 100
+  });
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+
+});
